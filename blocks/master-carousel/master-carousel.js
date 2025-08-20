67674090676bbe72a8a4976a41d07fbb5e021193
deepikaveer <veerasel@adobe.com>
2025-08-20 16:10:07 +0530

    (feat)video
    Add video style/JS files

--- blocks/master-carousel/master-carousel.js
+++ blocks/master-carousel/master-carousel.js
@@ -132,7 +132,6 @@ function createCountdownTimer(days, hours, minutes, seconds) {
  * @param {Element} block The block element
  */
 export default function decorate(block) {
-  return false;
   // Create container elements
   const container = document.createElement('div');
   container.className = 'masterCarousel-container';
